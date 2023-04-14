3278fde107fd545e6671e62db67eafcdbd5291c4
Gianluca <gianpetrii@gmail.com>
2023-04-14 13:16:41 -0300

    Comentarios en hello.c y archivo output creado

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,3 +1,14 @@
+/*
+   Utilizando VSC:
+
+    - Seleccionar Terminal > Run Build Task y si uso MinGW, luego seleccionar opcion 'C/C++: g++.exe build active file'
+    - Esto anterior crea el archivo '.exe'
+    - Ir a la terminal y pararse la carpeta del proyecto
+    - Correr el '.exe' desde la terminal, deberia decir 'Hello, World!'
+    - Una vez verificado esto, se escribe por terminar el comando './hello.exe > output.txt'
+    que deberia generar un archivo 'output.txt' con el texto 'Hello, World!'
+*/
+
 #include <stdio.h>
 
 int main(void)
